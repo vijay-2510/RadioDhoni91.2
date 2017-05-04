f851551f000c21956de5071740a97d2fed83c7ff
kumarvijay-msit <kumarvijay2510@gmail.com>
2017-05-04 21:58:26 +0530

    updated Image slider

---
+++ app/src/main/java/com/example/vijay/radiodhoni912/ViewPagerAdapter.java
@@ -0,0 +1,56 @@
+package com.example.vijay.radiodhoni912;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+/**
+ * Created by vijay on 5/4/2017.
+ */
+
+public class ViewPagerAdapter extends PagerAdapter {
+
+    private Context context;
+    private LayoutInflater layoutInflater;
+    private Integer[] images = {R.drawable.slide1,R.drawable.slide2,R.drawable.slide3,R.drawable.slide4};
+
+    public ViewPagerAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+
+        layoutInflater = (LayoutInflater)context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.custom_layout,null);
+        ImageView imageView = (ImageView)view.findViewById(R.id.imageView2);
+        imageView.setImageResource(images[position]);
+
+
+        ViewPager vp = (ViewPager)container;
+        vp.addView(view,0);
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        ViewPager vp = (ViewPager)container;
+        View view = (View)object;
+        vp.removeView(view);
+    }
+}
